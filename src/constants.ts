2be8e4c7f7ec405bc3c1fa1761e543e3e1576e22
Montejuado Michel <michel.montejuado@gmail.com>
2025-02-20 18:41:30 +0100

    Add navigation and top bar components; update site description and layout styles

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "Michel Montejuado";
-export const SITE_DESCRIPTION = "Bienvenue sur le site web de Michel Montejuado!";
+export const SITE_DESCRIPTION = "Le portfolio web de Michel Montejuado!";
